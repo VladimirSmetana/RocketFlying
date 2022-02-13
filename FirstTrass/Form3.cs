f8fdb0f535321a74ccd6ee632e4d293bef72d267
VladimirSmetana <98716102+VladimirSmetana@users.noreply.github.com>
2022-02-13 18:59:37 +0400

    Add files via upload

---
+++ FirstTrass/Form3.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace FirstTrass
+{
+
+    public partial class Form3 : Form
+    {
+        public static double w1;
+
+        
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new Form1().Show();
+            this.Hide();
+           // Form1 fm2 = new Form1();
+            w1 = Convert.ToDouble(textBox9.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+    }
+    static class FirstTrass
+    {
+
+    }
+}
